85fb3962aa6ce0841b1a08a990a20d4d505064c2
LinaeSostra <rebecca@dun.org>
2018-03-19 19:39:29 -0700

    RGBW Infrastructure

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_ssi.h"
 #include "inc/hw_types.h"
@@ -11,44 +13,36 @@
 #define LED_STRIP_PIN GPIO_PIN_5
 #define SSI_PINS GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_5
 
-#define BLUE_LED 4
-#define GREEN_LED 2
-#define RED_LED 1
+#define WHITE_LED_OFFSET 0
+#define BLUE_LED_OFFSET 1
+#define GREEN_LED_OFFSET 2
+#define RED_LED_OFFSET 3
 
-#define HIGH 1
-#define LOW 0
+#define NUM_OF_COLORS 4
 
 // Number of LEDs using on 1 meter 60-LED strip
 uint16_t ledCount = 12;
 
-// Color data. Order is GREEN,RED,BLUE;GREEN,RED,BLUE;...
-uint8_t * rgbBuffer;
-uint16_t rgbBufferLen;
+// Color data. Order is WHITE,GREEN,RED,BLUE; WHITE,GREEN,RED,BLUE;...
+uint8_t * colorData;
+uint16_t colorDataLen;
 
 // Groovy sine wave animation
-uint16_t atLED = 0;
-uint8_t color = 0;  // Bitmask: 0x1==red, 0x2==green, 0x4==blue
+uint8_t currentColor = 0x0;  // Bitmask: 0x1==red, 0x2==green, 0x4==blue
 
 // Convert each bit of 24-bit RGB color to 24 3-bit instructions.
 // Store in baudBuffer, then stream the baudBuffer via SPI.
 // Possible optimization: Generate these bytes in realtime during SPI transmission?
-uint8_t * baudBuffer;
-uint16_t baudBufferLen;
+uint8_t * pixels;
+uint16_t numBytes;
 
 void setup()
 {
-    rgbBufferLen = (ledCount+1)*3;
-    rgbBuffer = (uint8_t*) calloc( rgbBufferLen, sizeof(uint8_t) );
-    baudBufferLen = (rgbBufferLen) * 3;  // Every color bit becomes a 3-bit instruction, high [^^_] or low [^__]
-    baudBuffer = (uint8_t*) calloc( baudBufferLen, sizeof(uint8_t) );
 
-    //TODO(Rebecca): No idea what this is doing...
-    //pinMode(RED_LED, OUTPUT);
-    //pinMode(GREEN_LED, OUTPUT);
-    //pinMode(BLUE_LED, OUTPUT);
-
-    // Copied these commands from Energia's SPI.cpp library, and modified for custom clock speed:
-    unsigned long initialData = 0;
+    colorDataLen = (ledCount + 1);
+    colorData = (uint8_t*) calloc(colorDataLen, sizeof(uint32_t) ); // 32-bit color of each LED
+    numBytes = (colorDataLen) * NUM_OF_COLORS;
+    pixels = (uint8_t*) calloc( numBytes, sizeof(uint8_t) ); // bytes separated by WRGB of each LED
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -78,43 +72,80 @@ void setup()
     // Change the clock divider at your own peril :P The bit rate should already be correct.
     //mySPI.setClockDivider(SPI_CLOCK_DIV4);
     //HWREG( SSI2_BASE + SSI_O_CPSR ) = SPI_CLOCK_DIV4;
+
+    // Initialize each LED's color values to zero, and all the bytes of each color to zero
+    // Order of Bytes is W-G-R-B; W-G-R-B; etc
+    uint16_t ledCounter;
+    uint16_t byteCounter;
+    for(ledCounter = 0; ledCounter < colorDataLen; ledCounter++) {
+        currentColor = 0x0;
+        colorData[ledCounter] = 0x0;
+        for(byteCounter = 0; byteCounter < NUM_OF_COLORS; byteCounter++) {
+            uint16_t pixelsId = ledCounter * NUM_OF_COLORS + byteCounter;
+            pixels[pixelsId] = 0x0;
+        }
+    }
 }
+
+//TODO: Write a function for writing to an LED, setting a specific hue color, and reading an LED location
 int main(void)
 {
-uint16_t led;
-uint16_t rgbByteIdx;
-int8_t rgbBitIdx;
+uint16_t ledCounter;
+uint16_t byteCounter;
 uint16_t i;
+uint16_t aColor;
 
-static _Bool pulse = HIGH;
-static float wavePhase = 0.0;
 
 setup();
 while(1)
 {
+    // Convert the values in the colorData array to pixels array, and update the pixels array
+    for( ledCounter = 0; ledCounter < colorDataLen; ledCounter++) {
+        currentColor = colorData[ledCounter];
+        for(byteCounter = 0; byteCounter < NUM_OF_COLORS; byteCounter++) {
+            uint16_t pixelsId = ledCounter * NUM_OF_COLORS + byteCounter;
+            aColor = -1; // If see this in the code debugging, not going through the switch case
+            switch(byteCounter) {
+                case 0: // White
+                    aColor = (uint8_t) currentColor;
+                    break;
+                case 1: // Blue
+                    aColor = (uint8_t) currentColor >> 8;
+                    break;
+                case 2: // Green
+                    aColor = (uint8_t) currentColor >> 16;
+                    break;
+                case 3: // Red
+                    aColor = (uint8_t) currentColor >> 24;
+                    break;
+            }
+            pixels[pixelsId] = aColor;
+        }
+    }
     /*for(led = 0; led < ledCount; led++)
     {
         wavePhase += (0.02 / 6.0);  // Sine wave movement
 
-            uint16_t offset = led*3;
-            uint8_t bright = (uint8_t)((sin(led*0.3+wavePhase)+1.0)*0x7f);
-            uint8_t clr = (((led < atLED) ? (color+1) : (color)) % 7) + 1;
-            rgbBuffer[offset] = (clr&0x2) ? bright : 0;
-            rgbBuffer[offset+1] = (clr&0x1) ? bright : 0;
-            rgbBuffer[offset+2] = (clr&0x4) ? bright : 0;
-     }
+        uint16_t offset = led*4;
+        uint8_t bright = (uint8_t)((sin(led*0.3 + wavePhase) + 1.0)*0x7f);
+        uint8_t clr = (((led < atLED) ? (color + 1) : (color)) % 7) + 1;
+        rgbwBuffer[offset] = (clr&0x1) ? bright : 0;
+        rgbwBuffer[offset+1] = (clr&0x2) ? bright : 0;
+        rgbwBuffer[offset+2] = (clr&0x4) ? bright : 0;
+        rgbwBuffer[offset+3] = (clr&0x8) ? bright : 0;
+     }*/
 
     // Convert 8-bit RGB color to 3-baud instructions.
       // The WS2812 chips expect to receive pulse widths of 1/3 or 2/3:
       //   0: ^__  (low)
       //   1: ^^_  (HIGH)
-      uint16_t baudBufferIdx = 0;
+      /*uint16_t baudBufferIdx = 0;
       uint8_t baudByte = 0x0;  // Build an 8-bit byte, bitmasking with 3-bit ^__ and ^^_ instructions
 
       int8_t leftShift = 8;
 
-      for(rgbByteIdx = 0; rgbByteIdx < rgbBufferLen; rgbByteIdx++) {
-        uint8_t rgbByte = rgbBuffer[rgbByteIdx];
+      for(rgbByteIdx = 0; rgbByteIdx < rgbwBufferLen; rgbByteIdx++) {
+        uint8_t rgbByte = rgbwBuffer[rgbByteIdx];
 
         for(rgbBitIdx = 7; rgbBitIdx >= 0; rgbBitIdx-- ) {
           // For this bit: If it's a 1, use the pattern ^^_ ... 0 == ^__
@@ -142,24 +173,32 @@ while(1)
 
         }
       }*/
+        //uint8_t tempBaudByte = 0x0;
+          //uint32_t tempWGRBColor = 0x0;
+          //tempWGRBColor = tempWGRBColor | (0 << 24); //Red
+          //tempWGRBColor = tempWGRBColor | (0 << 16); //Blue
+          //tempWGRBColor = tempWGRBColor | (0 << 8); //Green
+          //tempWGRBColor = tempWGRBColor | (1); //White
+          /*tempBaudByte = tempBaudByte | (0b10 << 6);
+          tempBaudByte = tempBaudByte | (0b10 << 4);
+          tempBaudByte = tempBaudByte | (0b10 << 2);
+          tempBaudByte = tempBaudByte | (0b10);*/
+    for(i = 0; i < numBytes; i++) {
+        pixels[i] = 0;
+    }
+          pixels[0] = 0; // White
+          pixels[1] = 0; // Green
+          pixels[2] = 255; // Blue
+          pixels[3] = 0; // Red
+          printf(pixels[2]);
+          //baudBuffer[0] = 0x00;
+          //baudBuffer[1] = tempBaudByte;
 
       // Wait until SSI0_BASE is ready to send bytes
       while(SSIBusy( SSI0_BASE ));
-      uint8_t tempBaudByte = 0x0;
-      /*uint32_t tempWGRBColor = 0x0;
-      tempWGRBColor = tempWGRBColor | (0x00 << 24); //White
-      tempWGRBColor = tempWGRBColor | (0xFF << 16); //Green
-      tempWGRBColor = tempWGRBColor | (0x00 << 8); //Red
-      tempWGRBColor = tempWGRBColor | (0x00); //Blue*/
-      tempBaudByte = tempBaudByte | (0b10 << 6);
-      tempBaudByte = tempBaudByte | (0b10 << 4);
-      tempBaudByte = tempBaudByte | (0b10 << 2);
-      tempBaudByte = tempBaudByte | (0b10);
-      baudBuffer[0] = tempBaudByte;
-      //baudBuffer[0] = tempWGRBColor;
-      baudBuffer[1] = tempBaudByte;
-      for(i = 0; i < baudBufferLen; i++ ) {
-        uint8_t data = baudBuffer[i];
+
+      for(i = 0; i < numBytes; i++ ) {
+        uint8_t data = pixels[i];
 
         //unsigned long rxData;
         SSIDataPut( SSI0_BASE, data);
@@ -170,18 +209,16 @@ while(1)
       // Pause between animation frames.
       // WS2812 requires a 50us pause between frames to latch the color data.
       // delay(50) is 50 milliseconds, plenty of time.
-      // delay( 50 );
+      int timer;
+      for (timer = 0; timer < 5; timer++){}
 
-      /*// Animation: Advance to the next LED
-      atLED = (atLED+1) % ledCount;
-      if( atLED==0 ) {  // When we wrap around, advance the color
+      // Animation: Advance to the next LED
+      /*atLED = (atLED+1) % ledCount;
+      if( atLED == 0 ) {  // When we wrap around, advance the color
         color = (color+1)%7;
       }*/
 
-      // Blink the onboard LED to prove we're alive
-      //pulse = (pulse == HIGH) ? LOW : HIGH;
-
-      //   SSIDataPut(SSI0_BASE, ui32Data);
+      //SSIDataPut(SSI0_BASE, ui32Data);
 }
 
 }
