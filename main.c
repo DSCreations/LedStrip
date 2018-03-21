852d7c27b9364d468ab1a128f669638b36c42461
Jshimizu92 <37353127+Jshimizu92@users.noreply.github.com>
2018-03-20 18:57:47 -0700

    Added Interrupt Functions and Delay

--- main.c
+++ main.c
@@ -13,6 +13,9 @@
 #define LED_STRIP_PIN GPIO_PIN_5
 #define SSI_PINS GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_5
 
+#define MILLISECOND 24000
+#define WAIT_TIME 50 * MILLISECOND
+
 #define WHITE_LED_OFFSET 0
 #define BLUE_LED_OFFSET 1
 #define GREEN_LED_OFFSET 2
@@ -21,7 +24,7 @@
 #define NUM_OF_COLORS 4
 
 // Number of LEDs using on 1 meter 60-LED strip
-uint16_t ledCount = 12;
+#define LED_COUNT 60
 
 // Color data. Order is WHITE,GREEN,RED,BLUE; WHITE,GREEN,RED,BLUE;...
 uint16_t colorDataLen, numBytes;
@@ -29,7 +32,7 @@ uint8_t *pixels; // Convert each bit of 32-bit RGBW color to 4 8-bit instruction
 
 void setup()
 {
-    colorDataLen = ledCount + 1;
+    colorDataLen = LED_COUNT + 1;
     numBytes = colorDataLen * NUM_OF_COLORS;
     pixels = (uint8_t*) calloc( numBytes, sizeof(uint8_t) ); // bytes separated by WRGB of each LED
 
@@ -47,16 +50,16 @@ void setup()
     // SSI_FRF_MOTO_MODE_0 breaks the timing when the first LED is lit.
     // (Acts as if an extra 3-bit instruction is added to the beginning of the stream.)
     // Inverting the clock phase (SSI_FRF_MOTO_MODE_1) magically fixes the problem. Hardware bug?
-    SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_1, SSI_MODE_MASTER, 2150000, 8);
+    SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_1, SSI_MODE_MASTER, 2400000, 8);
 
     SSIEnable(SSI0_BASE);
 
     // Initialize each LED's color values to zero, and all the bytes of each color to zero
     // Order of Bytes is W-G-R-B; W-G-R-B; etc
-    uint16_t ledCounter, byteCounter, pixelsId;
-    for(ledCounter = 0; ledCounter < colorDataLen; ledCounter++) {
+    uint16_t LED_COUNTer, byteCounter, pixelsId;
+    for(LED_COUNTer = 0; LED_COUNTer < colorDataLen; LED_COUNTer++) {
         for(byteCounter = 0; byteCounter < NUM_OF_COLORS; byteCounter++) {
-            pixelsId = ledCounter * NUM_OF_COLORS + byteCounter;
+            pixelsId = LED_COUNTer * NUM_OF_COLORS + byteCounter;
             pixels[pixelsId] = 0x0;
         }
     }
@@ -64,7 +67,7 @@ void setup()
 
 // Given a red, green, blue, and white color value, set the pixels at led n to the specified colors
 void setPixelRGBWColor(uint16_t n, uint8_t red, uint8_t green, uint8_t blue, uint8_t white) {
-    if(n < ledCount) {
+    if(n < LED_COUNT) {
         uint16_t pixelsId = n * NUM_OF_COLORS;
         uint8_t *p = &pixels[pixelsId];
 
@@ -77,7 +80,7 @@ void setPixelRGBWColor(uint16_t n, uint8_t red, uint8_t green, uint8_t blue, uin
 
 // Given a RGBW aColor, set the pixels at led n to the specified colors
 void setPixelColor(uint16_t n, uint32_t aColor) {
-    if(n < ledCount) {
+    if(n < LED_COUNT) {
        uint8_t white, blue, green, red;
 
        uint16_t pixelsId = n * NUM_OF_COLORS;
@@ -102,7 +105,7 @@ void clearPixels(void) {
 
 // Get the clumped 32-bit RGBW color value at led n
 uint32_t getColor(uint16_t n) {
-    if (n >= ledCount) {
+    if (n >= LED_COUNT) {
         return 0;
     }
     uint32_t color = 0;
@@ -119,7 +122,7 @@ uint32_t getColor(uint16_t n) {
 
 // Helper Function for color retrieval at led n
 uint8_t getSpecificColor(uint16_t n, uint8_t offset) {
-    if (n >= ledCount) {
+    if (n >= LED_COUNT) {
         return 0;
     }
     uint16_t pixelsId = n * NUM_OF_COLORS;
@@ -153,32 +156,36 @@ uint8_t getRedColor(uint16_t n) {
  * (1) function to read value on the data bus
  * (2) interrupts to change the LED value
  */
+
+void showLEDStrip() {
+
+    // Disable interrupts
+   GPIOIntDisable(GPIO_PORTA_BASE, LED_STRIP_PIN);
+
+    // Cycles through Pixels and sets color values on LED Strip
+    uint16_t pixelIndex;
+    for (pixelIndex = 0; pixelIndex < numBytes; pixelIndex++) {
+        uint8_t aPixel = pixels[pixelIndex];
+        SSIDataPut(SSI0_BASE, aPixel);
+        while(SSIBusy(SSI0_BASE)){}
+    }
+
+   GPIOIntEnable(GPIO_PORTA_BASE, LED_STRIP_PIN);
+}
+
 int main(void) {
-    uint8_t i, timer;
 
     setup();
     //setPixelRGBWColor(0, 255, 255, 255, 255);
     //setPixelRGBWColor(1, 0, 0, 255, 0);
     //setPixelRGBWColor(6, 0, 255, 0, 0);
     //setPixelRGBWColor(3, 0, 0, 0, 0);
-    clearPixels();
-    //setPixelColor(1, 0xF000);
+   // clearPixels();
+    setPixelColor(10, 0xFFFF);
     //setPixelColor(2, 0x0F00);
     while(1)
     {
-      // Wait until SSI0_BASE is ready to send bytes
-      while(SSIBusy( SSI0_BASE ));
-
-      for(i = 0; i < numBytes; i++ ) {
-        uint8_t data = pixels[i];
-
-        //unsigned long rxData;
-        SSIDataPut( SSI0_BASE, data);
-      }
-
-      // Pause between animation frames.
-      // WS2812 requires a 50us pause between frames to latch the color data.
-      // delay(50) is 50 milliseconds, plenty of time.
-      for (timer = 0; timer < 5; timer++){}
+      showLEDStrip();
+      SysCtlDelay(WAIT_TIME);
     }
 }
